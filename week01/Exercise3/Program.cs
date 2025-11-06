1022f23a7df325e8df4fca67f19b1d4fadb216f8
Debs-droid <dstowell@byupathway.edu>
2025-11-06 14:00:28 -0500

    week 1 exercise 3 loops and random

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -3,7 +3,33 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+    {   //This section is for part 1 and 2
+        /*Console.Write("What is the magic number? ");
+        string magic = Console.ReadLine();
+        int magicNum = int.Parse(magic);*/
+
+        Random randomNum = new Random();
+        int magicNum = randomNum.Next(1, 100);
+        int guess = -1;
+
+        while (guess != magicNum)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNum > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNum < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed the magic number!");
+            }
+        }
+       
     }
 }
