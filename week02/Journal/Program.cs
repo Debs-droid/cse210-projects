500700a8b83c507e55c9c50da4d8c623c8d60b18
Debs-droid <dstowell@byupathway.edu>
2025-11-13 18:43:45 -0500

    stubs created after design

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,3 +1,44 @@
+/*
+Class: Program (the menu will be here, I think)
+Attributes:
+* _write : string
+*_display : string
+*_load : string
+*_save : string
+*_quit : string
+Behaviors:
+*SelectMenuItem() : void
+
+Class: Journal (will store a list--yes a LIST--of journal entries)
+Attributes:
+*_entry : List<Entry>
+Behaviors:
+*SaveToFile(file : string) void
+*LoadFromFile(file : string) void
+*DisplayAll(): void
+*AddEntry(newEntry : Entry) : void
+
+Class: Entry
+Attributes:
+*_date : string
+*_promptText : string
+*_entryText : string
+Behaviors:
+*Display(): void
+
+
+Class: PromptGenerator 
+Attributes:
+*_prompts : List<string>
+Behaviors:
+*GetRandomPrompt(): string
+*
+*/
+
+
+
+
+
 using System;
 
 class Program
