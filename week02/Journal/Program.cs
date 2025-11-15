ce3946ecf7e8e558c1445daf76e32e2ce8f48e98
Debs-droid <dstowell@byupathway.edu>
2025-11-15 18:13:09 -0500

    more work on journal project

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -3,50 +3,62 @@ using System.IO;
 class Program
 {
     static void Main(string[] args)
-       {
-            Journal j = new Journal();
+    {
+        Journal j = new Journal();
 
-            PromptGenerator p = new PromptGenerator();
-            
-            while (true)
+        PromptGenerator p = new PromptGenerator();
+
+        while (true)
+        {
+            Console.WriteLine("Welcome to the Journal Program! \nPlease select one of the following choices: ");
+            Console.WriteLine("1. Write\n2. Display\n3. Load\n4. Save\n5. Quit");
+            Console.WriteLine("What would you like to do? ");
+            int number = int.Parse(Console.ReadLine());
+
+
+            if (number == 1) //receive prompt and write in journal. press enter when finished
             {
-                Console.WriteLine("Welcome to the Journal Program! \nPlease select one of the following choices: ");
-                Console.WriteLine("1. Write\n2. Display\n3. Load\n4. Save\n5. Quit");
-                Console.WriteLine("What would you like to do? ");
-                int number = int.Parse(Console.ReadLine());
+                string _prompt = p.GetRandomPrompt();
+                Console.WriteLine($"{_prompt}");
+                Entry e = new Entry();
+                string _entry = Console.ReadLine();
+                e._entryText = _entry; 
+
+                e._promptText = _prompt; //e._promptText is the variable, _prompt (the data is in _prompt)
+                                
+                DateTime theCurrentTime = DateTime.Now;
+                string dateText = theCurrentTime.ToShortDateString();
+                e._date = dateText;
 
 
-                if (number == 1) //receive prompt and write in journal. press enter when finished?
-                {
-                    Console.WriteLine($"{p.GetRandomPrompt()}"); //add readline and 
-                    
-                }
-                else if (number == 2) //display all journal entries--iterate through and display all entries
-                {
-                    //Console.WriteLine($"Date: {date} - Prompt: {prompt}");
-                    //Console.WriteLine($"{/* prompt from text file?*/}");
-                }
-                else if (number == 3) //load journal entry  --do we have to load first, in order to display?
-                {
-                    Console.WriteLine("What is the file name?");
 
-                    string filename = Console.ReadLine();
-                }
-                else if (number == 4) //Save to textfile journal.txt
-                {
-                    SaveToFile(j._entries); 
-                }
-                else if (number == 5) //exit program--click run to start again
-                {
-                    System.Environment.Exit(1);
-                }
-            }    
+            }
+            else if (number == 2) //display all journal entries--iterate through and display all entries
+            {
+                //Console.WriteLine($"Date: {date} - Prompt: {prompt}");
+                //Console.WriteLine($"{/* prompt from text file?*/}");
+            }
+            else if (number == 3) //load journal entry  --do we have to load first, in order to display?
+            {
+                Console.WriteLine("What is the file name?");
+
+                string filename = Console.ReadLine();
+            }
+            else if (number == 4) //Save to textfile journal.txt
+            {
+                SaveToFile(j._entries);
+            }
+            else if (number == 5) //exit program--click run to start again
+            {
+                System.Environment.Exit(1);
+            }
         }
-        
+    }
+
 
     public static void SaveToFile(List<Entry> entry) //entry is the name it is referred to. j._entries is the object or particular instance
     {
-        
+
         Console.WriteLine("What is the file name?");
         string filename = Console.ReadLine();
 
